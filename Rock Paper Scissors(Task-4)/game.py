2947709f05066d2ccea37bacd2aad1ac46678549
gabhanerutvik-hash <gabhanerutvik@gmail.com>
2026-05-09 19:57:41 +0530

    Create game.py

---
+++ Rock Paper Scissors(Task-4)/game.py
@@ -0,0 +1,31 @@
+import random
+
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_choice = input("Enter rock, paper, or scissors: ").lower()
+    computer_choice = random.choice(options)
+
+    print("You chose:", user_choice)
+    print("Computer chose:", computer_choice)
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+
+    elif (
+        (user_choice == "rock" and computer_choice == "scissors") or
+        (user_choice == "paper" and computer_choice == "rock") or
+        (user_choice == "scissors" and computer_choice == "paper")
+    ):
+        print("You win!")
+
+    elif user_choice in options:
+        print("Computer wins!")
+
+    else:
+        print("Invalid input.")
+
+    again = input("Play again? (yes/no): ").lower()
+    if again != "yes":
+        print("Game ended.")
+        break
